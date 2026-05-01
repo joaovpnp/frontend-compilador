e5de9558c2ed15324a7341e5aff7996a7de0aa0f
joaovpnp <joaov.pnp@gmail.com>
2026-05-01 11:03:34 -0300

    Primeira versão do módulo Token

---
+++ src/Token.hs
@@ -0,0 +1,55 @@
+{--
+
+Este módulo visa definir todos os tokens da linguagem que serão retornados pelo Lexer para o Parser.
+
+--}
+
+module Token where
+
+data Token =
+    -- Palavras-chave
+    | IF                           -- if
+    | ELSE                         -- else
+    | WHILE                        -- while
+    | RETURN                       -- return
+    | PRINT                        -- print
+    | INT                          -- int
+    | DOUBLE                       -- double
+    | STRING                       -- string
+    | VOID                         -- void
+    -- Números
+    | NUMINT Int
+    | NUMDOUBLE Double
+    -- Identificador de variável
+    | ID String
+    -- Constante para strings
+    | LITERAL String
+    -- Operador de atribuicao
+    | ASSIGN                       -- =
+    -- Operadores aritméticos
+    | ADD                          -- +
+    | SUB                          -- -
+    | MUL                          -- *
+    | DIV                          -- /
+    -- Operadores relacionais
+    | EQ                           -- ==
+    | DIFF                         -- /=
+    | LT                           -- <
+    | GT                           -- >
+    | LE                           -- <=
+    | GE                           -- >=
+    -- Operadores lógicos
+    | AND                          -- &&
+    | OR                           -- ||
+    | NOT                          -- !
+    -- Chaves
+    | LCHAVE                       -- {
+    | RCHAVE                       -- }
+    -- Parênteses
+    | LPAR                         -- (
+    | RPAR                         -- )
+    -- Símbolos especiais
+    | COMMA                        -- ,
+    | NEWLINE                      -- \n
+    | CMDEND                       -- ;
+    deriving Show
