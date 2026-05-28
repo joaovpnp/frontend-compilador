d6df0ac12aaab547276548545237036c62aac52d
joaovpnp <joaov.pnp@gmail.com>
2026-05-28 19:39:07 -0300

    Adicionada assinatura de tipos

--- src/Semantico.hs
+++ src/Semantico.hs
@@ -22,21 +22,25 @@ warningMsg s = Result (False, "Advertencia:"++s++"\n", ())
 xor :: Bool -> Bool -> Bool
 xor a b = (a && not b) || (not a && b)
 
+isFuncaoDeclarada :: [Funcao] -> Id -> Bool
 isFuncaoDeclarada [] _ = False
 isFuncaoDeclarada ((id :->: (_,_)):xs) idFuncao
     | id == idFuncao = True
     | otherwise = isFuncaoDeclarada xs idFuncao
 
+isFuncaoDeclarada :: [Funcao] -> Id -> Bool
 isFuncaoDuplamenteDeclarada [] _ = False
 isFuncaoDuplamenteDeclarada ((id :->: (_,_)):xs) idFuncao
     | idFuncao == id = xor True (isFuncaoDuplamenteDeclarada xs idFuncao)
     | otherwise = xor False (isFuncaoDuplamenteDeclarada xs idFuncao)
 
+isFuncaoDeclarada :: [Var] -> Id -> Bool
 isVarDeclarada [] _ = False
 isVarDeclarada ((id :#: (_,_)):xs) idVariavel
     | id == idVariavel = True
     | otherwise = isVarDeclarada xs idVariavel
 
+isFuncaoDeclarada :: [Var] -> Id -> Bool
 isVarDuplamenteDeclarada [] _ = False
 isVarDuplamenteDeclarada ((id :#: (_,_)):xs) idVariavel
     | idVariavel == id = xor True (isVarDuplamenteDeclarada xs idVariavel)
