6a02fb67ea9f4d4de921af0c80f7faad3210d01a
joaovpnp <joaov.pnp@gmail.com>
2026-05-28 19:36:03 -0300

    Criadas funções simples de verificação

---
+++ src/Semantico.hs
@@ -0,0 +1,51 @@
+module Semantico where
+
+import AST
+
+data Result a = Result (Bool, String, a) deriving Show
+
+instance Functor Result where
+    fmap f (Result (b, s, a)) = Result (b, s, f a)
+
+instance Applicative Result where
+    pure a = Result (False, "", a)
+    Result (b1, s1, f) <*> Result (b2, s2, x) = Result (b1 || b2, s1 <> s2, f x)   
+
+instance Monad Result where 
+    -- return a = Result (False, "", a)
+    Result (b, s, a) >>= f = let Result (b', s', a') = f a in Result (b || b', s++s', a')
+  
+errorMsg s = Result (True, "Erro:"++s++"\n", ())
+
+warningMsg s = Result (False, "Advertencia:"++s++"\n", ())
+
+xor :: Bool -> Bool -> Bool
+xor a b = (a && not b) || (not a && b)
+
+isFuncaoDeclarada [] _ = False
+isFuncaoDeclarada ((id :->: (_,_)):xs) idFuncao
+    | id == idFuncao = True
+    | otherwise = isFuncaoDeclarada xs idFuncao
+
+isFuncaoDuplamenteDeclarada [] _ = False
+isFuncaoDuplamenteDeclarada ((id :->: (_,_)):xs) idFuncao
+    | idFuncao == id = xor True (isFuncaoDuplamenteDeclarada xs idFuncao)
+    | otherwise = xor False (isFuncaoDuplamenteDeclarada xs idFuncao)
+
+isVarDeclarada [] _ = False
+isVarDeclarada ((id :#: (_,_)):xs) idVariavel
+    | id == idVariavel = True
+    | otherwise = isVarDeclarada xs idVariavel
+
+isVarDuplamenteDeclarada [] _ = False
+isVarDuplamenteDeclarada ((id :#: (_,_)):xs) idVariavel
+    | idVariavel == id = xor True (isVarDuplamenteDeclarada xs idVariavel)
+    | otherwise = xor False (isVarDuplamenteDeclarada xs idVariavel)
+
+testarDeclaracaoFuncoes [] _ = False
+testarDeclaracaoFuncoes xs [] = True
+testarDeclaracaoFuncoes xs (y:ys) = isFuncaoDeclarada xs y && (not isFuncaoDuplamenteDeclarada  xs y) && testarDeclaracaoFuncoes xs ys
+
+testarDeclaracaoVars [] _ = False
+testarDeclaracaoVars xs [] = True
+testarDeclaracaoVars xs (y:ys) = isVarDeclarada xs y && (not isVarDuplamenteDeclarada  xs y) && testarDeclaracaoVars xs ys
